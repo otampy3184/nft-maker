72e1933f717d30db2805d123058d250e11bd7c84
django_freeman <o.tampodo.t.18@gmail.com>
2022-12-19 17:49:33 +0900

    ウォレット接続修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+interface Window {
+  ethereum: any;
+}
+
+declare var window: Window
+
+const App = () => {
   const CONTRACT_ADDRESS = "0xE2e0Cb146b13AA1C15a62e52Bc69D58496596438"
 
   const [currentAccount, setCurrentAccount] = useState("")
 
   console.log("currentAccount:", currentAccount);
 
-  const checkIfWalletConnected =async () => {
-    const { ethereum } = window as any
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window
     if (!ethereum) {
       console.log("metamask not found")
       return
     } else {
       console.log("Metamask founded")
-    } 
+    }
 
     const accounts = await ethereum.request({ method: "eth_accounts" })
     if (accounts.length !== 0) {
@@ -26,27 +32,19 @@ function App() {
     }
   }
 
-  const connectWallet =async () => {
-      try {
-        const { ethereum } = window as any
-        if (!ethereum) {
-          console.log("metamask not found")
-          return
-        } else {
-          console.log("Metamask founded")
-        } 
-    
-        const accounts = await ethereum.request({ method: "eth_accounts" })
-        if (accounts.length !== 0) {
-          console.log("connected:", accounts[0])
-          setCurrentAccount(accounts[0])
-        } else {
-          console.log("Account not found")
-        }
-      } catch (error) {
-        console.log(error)
-      }
+  const connectWallet = async () => {
+    const accounts = await window.ethereum.request({ method: "eth_requestAccounts" })
+    if (accounts.length !== 0) {
+      console.log("connected:", accounts[0])
+      setCurrentAccount(accounts[0])
+    } else {
+      console.log("Account not found")
+    }
   }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   const renderNotConnectedContainer = () => (
     <button onClick={connectWallet} className="cta-button connect-wallet-button">
@@ -65,7 +63,7 @@ function App() {
         )}
       </div>
     </div>
-    
+
   );
 }
 
