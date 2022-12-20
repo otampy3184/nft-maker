dd942fdc41d024c435a845e68568a10685712a5f
django_freeman <o.tampodo.t.18@gmail.com>
2022-12-20 19:38:45 +0900

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { useEffect, useState } from 'react'
 import NFTMaker from './abi/NFTMaker.json'
 import { ethers } from 'ethers'
+import { Web3Storage } from 'web3.storage'
 import './App.css'
-import { sign } from 'crypto'
 
 const App = () => {
+  const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDEyZUM3OTFBREM0NGYyMmI0ODlmNEYxQTk1ODk2ODM2M0RGRUVGNzAiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NjUyMzU4NjIzMTgsIm5hbWUiOiJuZnQtbWFrZXIifQ.ozxz5s4zkcGENyU9kr_pLRK1p4LBgqgGAULJRqcwxcQ";
   const CONTRACT_ADDRESS = "0xE2e0Cb146b13AA1C15a62e52Bc69D58496596438"
 
   const [currentAccount, setCurrentAccount] = useState("")
-
-  console.log("currentAccount:", currentAccount)
+  const [isLoading, setIsLoading] = useState(false);
 
   const checkIfWalletIsConnected = async () => {
     const { ethereum } = window
@@ -53,6 +53,24 @@ const App = () => {
     }
   }
 
+  const uploadToIpfs = async (e: any) => {
+    const client = new Web3Storage({ token: API_KEY })
+    const image = e.target
+
+    const rootCid = await client.put(image.file, {
+      name: 'metadata',
+      maxRetries: 3
+    })
+
+    const res = await client.get(rootCid)
+    const files = await res?.files()
+    if (files) {
+      for (const file of files) {
+        console.log("file.cid:", file.cid)
+      }
+    }
+  }
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
@@ -67,6 +85,15 @@ const App = () => {
     <button onClick={mintNft} className="cta-button">
       Mint NFT
     </button>
+  }
+
+  const renderUploadToIpfs = () => {
+    <div className="nftUplodeBox">
+      <div className="imageLogoAndText">
+        <p>ここにドラッグ＆ドロップ</p>
+      </div>
+      <input className="nftUploadInput" multiple name="imageURL" type="file" accept=".jpg , .jpeg , .png" onChange={uploadToIpfs} />
+    </div>
   }
 
   return (
